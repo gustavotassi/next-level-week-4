8eb3d9e457d98ea4ceb15c4aa7912b1063cbb410
Gustavo Tassi <souzagustavotassi@gmail.com>
2021-03-25 22:02:42 -0300

    feat(user): add user router

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata';
 import './database';
+import { router } from './routes';
 import express from 'express';
 
 const port = process.env.PORT || 3333;
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
