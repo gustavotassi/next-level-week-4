c2115a4e66ec8f9710708c14f2c53d4c4a2b1ee0
Gustavo Tassi <souzagustavotassi@gmail.com>
2021-03-29 23:09:45 -0300

    feat(survey): add surveys route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { UserController } from './controllers/UserController';
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
-router.post('/users', userController.create)
+router.post('/users', userController.create);
+router.post('/surveys', surveysController.create);
 
 export { router };
