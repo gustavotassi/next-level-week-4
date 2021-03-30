340e14d373f213f308b0cfdbfdd21e4ac970df3a
Gustavo Tassi <souzagustavotassi@gmail.com>
2021-03-29 23:10:31 -0300

    fix: update user controller to use custom repository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
-import { User } from './../models/User';
-import { request, Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 export class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
